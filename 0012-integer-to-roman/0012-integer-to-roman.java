8c7a230eafaff618cf4a0d87367d140f019cc06f
Shahnawaz Alam <74753709+Shahnawaz1913k@users.noreply.github.com>
2022-10-21 01:38:47 +0530

    Time: 6 ms (91.93%), Space: 41.9 MB (94.66%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String intToRoman(int num) {
+       String roman="";
+        int[] value = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] notation = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        
+        for(int pos = 0;num>0;++pos)
+        {
+            while(num>=value[pos])
+            {
+                roman+=notation[pos];
+                num-=value[pos];
+            }
+        }
+        return roman;
+    }
+}
